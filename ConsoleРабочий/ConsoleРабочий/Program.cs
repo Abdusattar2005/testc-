f04c35fd3aae7b9466e5d8c06ae2578a6a39d50c
Abdusattar2005 <abdaliuuluabdusattar2005@gmail.com>
2024-03-04 17:57:00 +0600

    fail

--- ConsoleРабочий/ConsoleРабочий/Program.cs
+++ ConsoleРабочий/ConsoleРабочий/Program.cs
@@ -1,22 +1,31 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main()
     {
-        int a = 5;
+        string aa;
+        Console.WriteLine("добро пожаловат ");
+        Console.ReadLine();
+        Console.WriteLine("как вас завут");
+        aa=Console.ReadLine();
+        Console.WriteLine("как вам помоч   "+aa);
+        Console.ReadLine();
+        Console.WriteLine("хорошо");
+        string ff;
+        int a, b;
+        Console.WriteLine("ведите свой цифры");
+        ff = Console.ReadLine();
+        a = Convert.ToInt32(ff);
+        Console.WriteLine("ведите свой второй цифры");
+        ff = Console.ReadLine();
+        b = Convert.ToInt32(ff);
+        int result = a * b;
+        Console.WriteLine( result);
+        
+        
 
-        Ahalaimahalai(a);
-        Console.WriteLine(a);
 
-        void Ahalaimahalai(int namber)
-        {
-            /*...*/
-            namber = 10;
-                /*...*/
-                namber++;
-        }
-            
     }
 }
-        
