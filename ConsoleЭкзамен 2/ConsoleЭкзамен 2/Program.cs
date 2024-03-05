19163cd7a178cef5745b21241aa408ca3c544e72
Abdusattar2005 <abdaliuuluabdusattar2005@gmail.com>
2024-03-05 19:01:29 +0600

    fail

--- ConsoleЭкзамен 2/ConsoleЭкзамен 2/Program.cs
+++ ConsoleЭкзамен 2/ConsoleЭкзамен 2/Program.cs
@@ -1,10 +1,43 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        
+        // 1
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        // 2
+        Console.WriteLine("Исходный список:");
+        PrintList(numbers);
+
+        // 3
+        numbers = AddValueToList(numbers, 6);
+
+        // 4
+        Console.WriteLine("\nСписок после добавления нового значения:");
+        PrintList(numbers);
+
+        Console.ReadLine();
+    }
+
+    // 5
+    static List<int> AddValueToList(List<int> list, int newValue)
+    {
+        // 6
+        list.Add(newValue);
+        return list;
+
+    }
+
+    // 7
+    static void PrintList(List<int> list)
+    {
+        // 8
+        foreach (var item in list)
+        {
+            Console.Write(item + " ");
+        }
     }
 }
