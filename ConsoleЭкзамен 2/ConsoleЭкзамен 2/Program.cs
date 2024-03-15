817aa36be7187dad4a4cff37061b8dab0d54abd0
Abdusattar2005 <abdaliuuluabdusattar2005@gmail.com>
2024-03-15 18:29:01 +0600

    fail

--- ConsoleЭкзамен 2/ConsoleЭкзамен 2/Program.cs
+++ ConsoleЭкзамен 2/ConsoleЭкзамен 2/Program.cs
@@ -6,7 +6,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        string ff = "waefr";
+        string ff;
+        int a, d, f;
+        Console.WriteLine("привет");
+        ff = Console.ReadLine();
+        a = Convert.ToInt32(ff);
+        Console.WriteLine(" ведите сифры");
+        ff = Console.ReadLine();
+        d = Convert.ToInt32(ff);
+        Console.WriteLine("ведите сифры");
+        ff = Console.ReadLine();
+        f = Convert.ToInt32(ff);
+        int result = a *(d +f);
+        Console.WriteLine(result);
+
+
 
     }
 }
